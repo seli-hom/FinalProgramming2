919d186b164cfe7942d6cb3f61edbc374a97e918
%username% <%email%>
2024-10-26 19:02:10 -0400

    Addition of attributes and methods for StudentManager

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -1,5 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class StudentManager {
-    private
+    private ArrayList<Student> students;
+
+    /**
+     * Adds Student to the array list of students
+     * @param student
+     */
+    public void addStudent(Student student){
+
+    }
+
+    /**
+     * Removes student from array list of students
+     * @param studId
+     */
+    public void removeStudent(int studId){
+
+    }
+
+    /**
+     * Searches for the Student through their studentID
+     * @param studId
+     * @return the Student corresponding to the id
+     */
+    public Student searchStudentById(int studId){
+
+    }
 }
