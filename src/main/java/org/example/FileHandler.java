8756e41369388937a72267dfbb6510a87f280d60
%username% <%email%>
2024-10-26 19:08:27 -0400

    Creation of InterFace Class File Handler and addition of static methods

---
+++ src/main/java/org/example/FileHandler.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public interface FileHandler {
+    public static void saveToFile(String file, ArrayList<Student> students){
+
+    }
+    public static void loadFromFile(String file){
+
+    }
+}
