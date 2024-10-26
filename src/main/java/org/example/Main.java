188478bd16f89501b52bde87dd22811bf4a1bc07
%username% <%email%>
2024-10-26 19:16:41 -0400

    No additions just comments about what is needed in the Main GUI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Main GUI:
+        //Text field
+        //Table view
+        //Buttons
+        //Alert Dialogs
     }
 }
