81c8b0442567800efc3d3b461709329cbfa298b0
Selihom <efremSelihom1@gmail.com>
2024-11-07 13:24:20 -0500

    Implemented constructor for Student Class and StudentManager Class added code for methods in Student manager

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,13 +1,38 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+@Getter
 public class Student extends Person{
     private int studId;
+    @Setter
     private double studGPA;
-    private int nextID = 10001;
+    private static int nextID = 10001;
+
+    public Student() {
+    }
+
+    public Student(int studId, double studGPA) {
+        this.studId = nextID++;
+        this.studGPA = studGPA;
+
+    }
 
     @Override
     public void getDescription() {
         System.out.println("Student: " + studId + "\n"
                 + getFName() + " " + getLNAme());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return studId == student.studId;
+    }
+
 }
--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -1,24 +1,43 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
+@Setter
+@Getter
 public class StudentManager {
     private ArrayList<Student> students;
+
+    public StudentManager() {
+        this.students = new ArrayList<Student>();
+    }
+
+    public StudentManager(ArrayList<Student> students) {
+        this.students = students;
+    }
 
     /**
      * Adds Student to the array list of students
      * @param student
      */
     public void addStudent(Student student){
-
+    students.add(student);
     }
 
     /**
      * Removes student from array list of students
      * @param studId
      */
-    public void removeStudent(int studId){
-
+    public void removeStudent(int studId) throws StudNotFoundException {
+        Student toBeRemoved = searchStudentById(studId);
+        if (toBeRemoved == null){
+            System.out.println("Student does not exist in the system" +
+                    '\n' + "Action not performed...");
+            return;
+        }
+        students.remove(toBeRemoved);
     }
 
     /**
@@ -26,7 +45,25 @@ public class StudentManager {
      * @param studId
      * @return the Student corresponding to the id
      */
-    public Student searchStudentById(int studId){
-
+    public Student searchStudentById(int studId) throws StudNotFoundException {
+        return searchStudentById(studId,0);
     }
+
+    /**
+     * Helper method for recursion. Searches for thestudent by going through all students and finding a
+     * match with the studentId we are searching for, if not found it will throw a StudentNotFound Exception.
+     * @param studId student we are looking for
+     * @param idx initial idx comparing the studentIds
+     * @return Student that matches the studentId input
+     */
+    private Student searchStudentById(int studId, int idx) throws StudNotFoundException{
+        for (Student student :  students){
+            if (student.getStudId() == studId) {
+                return student;
+            }
+            searchStudentById(studId, idx + 1);
+        }
+        throw new StudNotFoundException();
+    }
+
 }
