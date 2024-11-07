25726ed9de93f224a9a068de72e06b9d70b406dd
Selihom <efremSelihom1@gmail.com>
2024-11-07 13:35:49 -0500

    Added constructor for following classes: IOException, NumberFormatException, StudentNotFoundExceptio. modified Person getDescripion method to be abstract.

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -11,7 +11,5 @@ abstract class Person {
     private String fName;
     private String lNAme;
 
-    abstract public void getDescription(){
-        System.out.println("Name: " + fName + " " + lNAme);
-    }
+    abstract public void getDescription();
 }
--- src/main/java/org/example/StudNotFoundException.java
+++ src/main/java/org/example/StudNotFoundException.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class StudNotFoundException extends Exception{
+    public StudNotFoundException() {
+        super("Student is not found in the system");
+    }
+
+    public StudNotFoundException(String message) {
+        super(message);
+    }
 }
