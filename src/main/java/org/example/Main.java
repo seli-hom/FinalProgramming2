b4fc40cc06433c7f79fea879f51d0f02cbb3ce8d
Selihom <efremSelihom1@gmail.com>
2024-11-08 21:42:13 -0500

    Menu page,  plus switch case for different choices added to the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,21 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws StudNotFoundException {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
 
         //Main GUI:
-        //Text field
         //Table view
         //Buttons
         //Alert Dialogs
-
+// premade students to test load and save file methods
         Student tony = new Student("Tony", "Effe",3.1 );
         Student poppy = new Student("Poppy", "Seeds",2.6 );
         Student julie = new Student("Julie", "Plante",3.0 );
-        ArrayList<Student> students;
-        students = new ArrayList<Student>();
-        students.add(tony);
-        students.add(poppy);
+        ArrayList<Student> students = new ArrayList<Student>();
 
         StudentManager school = new StudentManager(students);
         school.addStudent(julie);
@@ -27,14 +24,58 @@ public class Main {
 
 
         // creating user interaction
+        int choice = 0;
 
+        menuPage();
+        while (true){
 
+            switch(choice){
+                case 1 -> {
+                    console.nextLine();
+                    Student studentInput = new Student();
+                    System.out.println("Please input student first name: ");
+                    studentInput.setFName(console.nextLine());;
+                    System.out.println("Please input student last name: ");
+                    studentInput.setLNAme(console.nextLine());
+                    System.out.println("Please input student GPA: ");
+                    studentInput.setStudGPA(console.nextDouble());
 
+                    school.addStudent(studentInput);
+                    System.out.println("Student successfully added");
+                }
+                case 2 -> {
+                    console.nextLine();
+                    System.out.println("Please write the student Id of the student you wish to remove: ");
+                    int idToRemove = console.nextInt();
+                    school.removeStudent(idToRemove);
+                    System.out.println("Student removed successfully");
+                }
+                case 3 -> {
+                    console.nextLine();
+                    System.out.println("Please write the name of the file with '.txt' from which you wish to load from");
+                    school.loadFromFile(console.nextLine());
+                }
+                case 4 ->{
+                    console.nextLine();
+                    System.out.println("Please write the name of the file with '.txt' in which you wish to save the files");
+                    System.out.println("File saved successfully");
+                }
+            }
+            System.out.println("Would you like to perform another action? Yes/No");
+            console.nextLine();
 
+            String yesNo = console.nextLine();
+            if (!yesNo.equalsIgnoreCase("yes")) {
+                System.out.println("Exiting program...");
+                break;
+            } else {
+                menuPage();
+                choice = console.nextInt();
+                console.nextLine();
+            }
+        }
 
-
-
-
+//part of added objects and usage of methods for testing
         try {
           Student found =  school.searchStudentById(10004);
           if (found == null){
